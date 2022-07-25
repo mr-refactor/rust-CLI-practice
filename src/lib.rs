f4c0336944a9de749ee686d46702d3418e8ebcd6
Tr1ckC0 <trick.co@yahoo.com>
2022-07-24 23:27:46 -0500

    added search() fn to run() - program is functioning

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,10 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
+
     Ok(())
 }
 
@@ -31,7 +35,7 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
             results.push(line);
         }
     }
-    
+
     results
 }
 
