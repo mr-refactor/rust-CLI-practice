01ed8f2e2a19f7df506854694e1bdf546ec3ec31
Tr1ckC0 <trick.co@yahoo.com>
2022-07-24 22:51:19 -0500

    improved Config error handling and refactored login into run() function

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
+    run(config);
+}
+
+fn run(config: Config) {
     let contents = fs::read_to_string(config.filename)
         .expect("Something went wrong reading the file");
 
@@ -17,14 +25,17 @@ fn main() {
 
 struct Config {
     query: String,
-    filename: String
+    filename: String,
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
         let query = args[1].clone();
         let filename = args[2].clone();
-    
-        Config {query, filename}
+
+        Ok(Config { query, filename })
     }
 }
